c44bd028c7015fef8d0bf5cced8208a28019697d
coIorbIind <labinfpin13@gmail.com>
2022-07-17 12:01:13 +0300

    Base doc
    Added documentation to the BaseParser class. Added abstract method for parsing data

--- base_class.py
+++ base_class.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod
+from datetime import datetime
 
 
 class MetaSingleton(type):
+    """Метакласс, определяющий поведение singleton'a"""
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
@@ -13,9 +15,7 @@ class MetaSingleton(type):
 class BaseParser(metaclass=MetaSingleton):
     """Базовый класс для парсеров различных сайтов"""
 
-    # __metaclass__ = MetaSingleton
-
-    def __init__(self, url: str,):
+    def __init__(self, url: str):
         """
         Конструктор класса BaseParser.
         :param url: адрес сайта для парсинга.
@@ -23,8 +23,28 @@ class BaseParser(metaclass=MetaSingleton):
         self.__url = url
 
     def __str__(self):
+        """Строковое представление объекта"""
         return f"Парсер для сайта {self.__url}."
 
+    def __repr__(self):
+        """Строковое представление объекта"""
+        return f"Парсер для сайта {self.__url}."
+
+    @property
+    def url(self):
+        """Getter для url"""
+        return self.__url
+
+    @url.setter
+    def url(self, new_url):
+        """Setter для url"""
+        self.__url = new_url
+
     @abstractmethod
-    def parse(self):
+    def collect_data(self, date_from: datetime, date_to: datetime) -> str:
+        """
+        Метод для парсинга информации за некоторый промежуток времени.
+        :param date_from: дата начала сбора информации.
+        :param date_to: дата окончания сбора информации.
+        """
         pass
